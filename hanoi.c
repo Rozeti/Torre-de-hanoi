0fe999a9fb0b130e9d71f3b32ffc487b0679820f
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2024-06-20 15:38:51 -0300

    Inclusão das funções push e pop

--- hanoi.c
+++ hanoi.c
@@ -7,3 +7,16 @@
 void iniciarTorre(Torre *torre) {
     torre->topo = -1;
 }
+
+void push(Torre *torre, int disco) {
+    if (torre->topo < MAX_DISKS - 1) {
+        torre->discos[++torre->topo] = disco;
+    }
+}
+
+int pop(Torre *torre) {
+    if (torre->topo >= 0) {
+        return torre->discos[torre->topo--];
+    }
+    return -1;
+}
